bda8976001091e44357d5598cb1677cfc12adfab
Magakia <33makaki@gmail.com>
2024-09-01 02:01:20 +0300

    Add files via upload

---
+++ les2 variables.py
@@ -0,0 +1,5 @@
+quantity_compl_homework=12
+quantity_hour=1.5
+course_name='Python'
+one_task_time=quantity_hour/quantity_compl_homework
+print(course_name,', всего задач:',quantity_compl_homework,', затрачено часов: ',quantity_hour,', среднее время выполнения ', one_task_time,' часа.')
