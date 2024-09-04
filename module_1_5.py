423e6652de5d9dc39976a3e2155127698b1be844
Magakia <33makaki@gmail.com>
2024-09-05 01:14:11 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var= ("Осень",2,3,True)
+print(immutable_var)
+# immutable_var[0]="Весна"
+# print(immutable_var)
+mutable_list=["Осень",2,3,True]
+mutable_list[0]='Весна'
+mutable_list[1]=10
+mutable_list.append(99)
+print(mutable_list)
